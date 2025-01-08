5b6a6042ff4e1de9d4d8023a93341d39cb3740f8
Philip30 <141637056+philipo30@users.noreply.github.com>
2025-01-08 16:31:14 +0100

    Update footer.tsx

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -7,6 +7,7 @@ import {
   FaInstagram,
   FaRss,
   FaLinkedinIn,
+  FaDiscord,
 } from "react-icons/fa6";
 import { TbMailFilled } from "react-icons/tb";
 import { metaData, socialLinks } from "app/config";
@@ -24,10 +25,11 @@ function SocialLink({ href, icon: Icon }) {
 function SocialLinks() {
   return (
     <div className="flex text-lg gap-3.5 float-right transition-opacity duration-300 hover:opacity-90">
-      <SocialLink href={socialLinks.twitter} icon={FaXTwitter} />
+      {/*<SocialLink href={socialLinks.twitter} icon={FaXTwitter} />*/}
       <SocialLink href={socialLinks.github} icon={FaGithub} />
-      <SocialLink href={socialLinks.instagram} icon={FaInstagram} />
-      <SocialLink href={socialLinks.linkedin} icon={FaLinkedinIn} />
+      {/*<SocialLink href={socialLinks.instagram} icon={FaInstagram} />*/}
+      {/*<SocialLink href={socialLinks.linkedin} icon={FaLinkedinIn} />*/}
+      <SocialLink href={socialLinks.discord} icon={FaDiscord} />
       <SocialLink href={socialLinks.email} icon={TbMailFilled} />
       <a href="/rss.xml" target="_self">
         <FaRss />
